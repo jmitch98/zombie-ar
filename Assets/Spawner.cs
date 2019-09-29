6db001632344039e0340704c807387f39303afd2
James Mitchell <hello@jpmitchell.me>
2019-09-29 18:17:52 -0400

    initial

---
+++ Assets/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public GameObject zombie;
+    public float spawnTime = 1f;
+    private GameObject _target;
+
+    void Awake() {
+        _target = GameObject.FindGameObjectWithTag("Destination");
+    }
+
+    void Start() {
+        InvokeRepeating("SpawnZombie", spawnTime, spawnTime);
+    }
+
+    void SpawnZombie() {
+        float angle = Random.Range(-Mathf.PI, Mathf.PI);
+        Vector3 pos = _target.transform.position;
+        pos += new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * 3;
+        pos.y = 0;
+        GameObject z = Instantiate(zombie, gameObject.transform.TransformPoint(pos), Quaternion.identity);
+        z.transform.parent = gameObject.transform;
+    }
+}
---
+++ Assets/ZombieMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMove : MonoBehaviour {
+    public float MoveSpeed = 3f;
+    private GameObject _target;
+
+    void Awake() {
+        _target = GameObject.FindGameObjectWithTag("Destination");
+    }
+
+    void Update() {
+        if (transform.position == _target.transform.position) {
+            Destroy(gameObject);
+            Debug.Log("Zombie reached EOL");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, MoveSpeed * Time.deltaTime);
+    }
+}
